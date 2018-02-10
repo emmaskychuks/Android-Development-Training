2dc8a333012872dfa8cac97f94e813e49c6bc579
Emmanuel Obi <emmaskychuks@gmail.com>
2018-02-09 21:32:25 -0500

    UI/Ux polished, development on functionality started.

--- SlotFun/app/src/main/java/com/example/emmasky/slotfun/MainActivity.java
+++ SlotFun/app/src/main/java/com/example/emmasky/slotfun/MainActivity.java
@@ -1,9 +1,21 @@
 package com.example.emmasky.slotfun;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText amountInput;
+    private TextView accountDisplay;
+    private TextView reel1Display;
+    private TextView ree21Display;
+    private TextView reel3Display;
+    private Button newGameButton;
+    private Button setValueButton;
+    private Button pullLeverButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
